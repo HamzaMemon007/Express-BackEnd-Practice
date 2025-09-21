2137346784f3318647351da5f9b221a67f2b695c
HamzaMemon007 <hamzagamer059@gmail.com>
2025-09-21 21:44:31 +0500

    connected with mongoDB

--- 04-mongoDB-crud/index.js
+++ 04-mongoDB-crud/index.js
@@ -1,9 +1,9 @@
 import express from "express"
-import { nanoid } from "nanoid"
 import fs from 'fs'
 import path from 'path'
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Post from "./models/Post.js";
 
 
 
@@ -21,8 +21,6 @@ app.use(express.json())
 const connectDB = async () => {
     if (!MONGO_URI) throw new Error("MONGO_URI not defined in .env")
     try {
-        console.log(MONGO_URI);
-
         await mongoose.connect(MONGO_URI)
         console.log("Connected to DataBase Successfully")
     } catch (error) {
@@ -39,23 +37,22 @@ const connectDB = async () => {
     text:"abc"
     }*/
 
-const readPosts = () => {
-    if (!fs.existsSync(filePath)) {
-        fs.writeFileSync(filePath, JSON.stringify([]));
-    }
-    const data = fs.readFileSync(filePath)
-    return JSON.parse(data)
-}
+// const readPosts = () => {
+//     if (!fs.existsSync(filePath)) {
+//         fs.writeFileSync(filePath, JSON.stringify([]));
+//     }
+//     const data = fs.readFileSync(filePath)
+//     return JSON.parse(data)
+// }
 
-const writePosts = (posts) => {
-    fs.writeFileSync(filePath, JSON.stringify(posts, null, 2))
-}
+// const writePosts = (posts) => {
+//     fs.writeFileSync(filePath, JSON.stringify(posts, null, 2))
+// }
 
-const posts = readPosts()
 
 
 //Create POST
-app.post('/post', (req, res) => {
+app.post('/post', async (req, res) => {
     try {
         const { title, text } = req.body
         if (!title || !text) {
@@ -63,14 +60,8 @@ app.post('/post', (req, res) => {
                 error: "Title and Text are Required"
             })
         }
-        const newPost = {
-            id: nanoid(),
-            title,
-            text,
-        }
-        posts.push(newPost)
-        writePosts(posts)
-        res.status(201).json(newPost);
+        const post = await Post.create({title,text})
+        res.status(201).json(post);
     } catch (error) {
         console.error("Error creating post:", error);
         res.status(500).json({
@@ -82,9 +73,10 @@ app.post('/post', (req, res) => {
 
 // Get POSTS
 
-app.get('/posts', (req, res) => {
+app.get('/posts', async (req, res) => {
     try {
-        res.status(200).json(posts)
+        const post = await Post.find().sort({createdAt: -1})
+        res.status(200).json(post)
     } catch (error) {
         console.error("Error fetching post:", error);
         res.status(500).json({
@@ -96,11 +88,11 @@ app.get('/posts', (req, res) => {
 
 
 //Get Single POST
-app.get('/post/:postId', (req, res) => {
+app.get('/post/:postId', async (req, res) => {
     try {
         let postId = req.params.postId
 
-        const post = posts.find((p) => p.id === postId)
+        const post = await Post.findById(postId)
         if (!post) {
             return res.status(404).send({
                 "error": `post not found with id: ${postId}`
@@ -110,7 +102,7 @@ app.get('/post/:postId', (req, res) => {
 
 
     } catch (error) {
-        console.error(`Error fetching post: ${postId}`, error);
+        console.error(`Error fetching post: ${req.params.id}`, error);
         res.status(500).json({
             error: "Internal Server Error"
         })
@@ -120,26 +112,31 @@ app.get('/post/:postId', (req, res) => {
 
 
 // Update A Post
-app.put('/post/:postId', (req, res) => {
+app.put('/post/:postId', async (req, res) => {
     try {
         const { title, text } = req.body
-        let postId = req.params.postId
+        let postId = req.params.postId;
 
-        const post = posts.find((p) => p.id === postId)
+         if (!title || !text) {
+            return res.status(400).json({
+                error: "Title and Text are Required"
+            })
+        }
+
+        const post = await Post.findByIdAndUpdate(postId,{title, text})
         if (!post) {
             return res.status(404).send({
                 "error": `post not found with id: ${postId}`
             })
         }
 
-        if (title) post.title = title
-        if (text) post.text = text
-        writePosts(posts)
+        
+        
         res.json(post)
 
 
     } catch (error) {
-        console.error(`Error fetching post: ${postId}`, error);
+        console.error(`Error fetching post: ${req.params.id}`, error);
         res.status(500).json({
             error: "Internal Server Error"
         })
@@ -147,15 +144,15 @@ app.put('/post/:postId', (req, res) => {
 })
 
 
-app.delete('/post/:postId', (req, res) => {
+app.delete('/post/:postId', async (req, res) => {
     try {
         let postId = req.params.postId
+        const post = await Post.findByIdAndDelete(postId)
+        console.log(`Post with Id: ${req.params.postId} is deleted`)
+        res.status(204).json({
+            message: "Deleted Success Fully"
+        })
 
-        const postIndex = posts.findIndex((p) => p.id === postId)
-        if (postIndex === -1) return res.status(404).json({ error: "Post not found" });
-        const deletedPost = posts.splice(postIndex, 1);
-        writePosts(posts)
-        res.json({ message: "Post deleted", post: deletedPost[0] });
 
     } catch (error) {
         console.error(`Error fetching post`, error);
