622765c603a2ac02e65feeebe53abc68d1fb87a7
HamzaMemon007 <hamzagamer059@gmail.com>
2025-09-29 21:13:26 +0500

    Compleated

--- 05-Mongo-DB-By-Hamza/index.js
+++ 05-Mongo-DB-By-Hamza/index.js
@@ -29,10 +29,10 @@ const connectDb = async () => {
 
 app.post("/post", async (req, res) => {
     try {
-        const { title , text } = req.body
-        if(!title || !text) {
+        const { title, text } = req.body
+        if (!title || !text) {
             return res.status(400).json({
-                error : "Tittle & Text are Required"
+                error: "Tittle & Text are Required"
             })
         }
         const post = await Post.create({
@@ -41,31 +41,96 @@ app.post("/post", async (req, res) => {
         })
         res.status(201).json(post)
     } catch (error) {
-        console.error("Contact Your Developer For More Info : Error While Creating Posts" , error)
+        console.error("Contact Your Developer For More Info : Error While Creating Posts", error)
         res.status(500).json({
-            error : "Internal Server Error"
+            error: "Internal Server Error"
         })
-
     }
 })
 
 
-app.get('/posts' , async (req , res) =>{
+
+app.get('/posts', async (req, res) => {
     try {
-        const post = await Post.find().sort({createdAt : -1})
+        const post = await Post.find().sort({ createdAt: -1 })
         res.status(200).json(post)
     } catch (error) {
-        console.error("Contact Your Developer For More Info : Error While Getting Posts" , error)
+        console.error("Contact Your Developer For More Info : Error While Getting Posts", error)
         res.status(500).json({
-            error : "Internal Server Error"
+            error: "Internal Server Error"
         })
     }
 })
 
 
-app.listen(PORT, () => {
-    console.log(`App is Running on PORT ${PORT}`);
 
+app.get('/post/:postId', async (req, res) => {
+    try {
+        let postId = req.params.postId
+        const post = await Post.findById(postId)
+        if (!post) {
+            return res.status(404).send({
+                "error": `Recheck You Id Once: ${postId}`
+            })
+        }
+        res.status(200).json(post)
+
+    } catch (error) {
+        console.error(`Error fetching post: ${req.params.id}`, error);
+        res.status(500).json({
+            error: "Internal Server Error"
+        })
+    }
+})
+
+
+
+app.put('/post/:postId', async (req, res) => {
+    try {
+        const { title, text } = req.body
+        let postId = req.params.postId;
+        if (!title || !text) {
+            return res.status(400).json({
+                error: "Title and Text are Required"
+            })
+        }
+        const post = await Post.findByIdAndUpdate(postId, { title, text })
+        if (!post) {
+            return res.status(404).send({
+                "error": `post not found with id: ${postId}`
+            })
+        }
+        res.json(post)
+
+    } catch (error) {
+        console.error(`Error fetching post: ${req.params.id}`, error);
+        res.status(500).json({
+            error: "Internal Server Error"
+        })
+    }
+})
+
+app.delete('/post/:postId', async (req, res) => {
+    try {
+        let postId = req.params.postId
+        const post = await Post.findByIdAndDelete(postId)
+        console.log(`Post with Id: ${req.params.postId} is deleted`)
+        res.status(204).json({
+            message: "Deleted Success Fully"
+        })
+    } catch (error) {
+        console.error(`Error fetching post`, error);
+        res.status(500).json({
+            error: "Internal Server Error"
+        })
+    }
+})
+
+
+
+app.listen(PORT, async () => {
+    await connectDb();
+    console.log(`App is Running on PORT ${PORT}`);
 })
 
 
